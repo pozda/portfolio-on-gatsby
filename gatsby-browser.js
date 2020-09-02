5e20dd3ccf4f0404729ce587b64e87b3b2dd62a6
Ivan Pozderac <ivan.pozderac@gmail.com>
2020-09-02 02:19:43 +0100

    add loader if needed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+export const onInitialClientRender = () => {
+    setTimeout(function() {
+        document.getElementById("___loader").style.display = "none"
+    }, 1000)
+}
